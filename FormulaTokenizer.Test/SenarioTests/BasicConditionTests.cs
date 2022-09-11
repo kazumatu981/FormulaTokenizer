697e59049bec719b752c6fd020b89164b82fb60d
kazumatu981 <kazumatu981@gmail.com>
2022-09-11 19:42:48 +0900

    Added Senario Test (Normal Cases).

--- FormulaTokenizer.Test/SenarioTests/BasicConditionTests.cs
+++ FormulaTokenizer.Test/SenarioTests/BasicConditionTests.cs
@@ -18,7 +18,20 @@ public class BasicConditionTests
     [InlineData("8+2*4", 16)]
     // 四項計算
     [InlineData("8+2*4+5", 21)]
-    public void NoWhiteSpaces(string formula, int expected)
+    [InlineData("8/2+4*5", 24)]
+    // マイナス
+    [InlineData("-1+2", 1)]
+    [InlineData("-1+-2", -3)]
+    [InlineData("-1*-2", 2)]
+    [InlineData("-4/2", -2)]
+    // スペースあり
+    [InlineData(" 1+2", 3)]
+    [InlineData("9    *9", 81)]
+    [InlineData("8/  2", 4)]
+    [InlineData("8-2    ", 6)]
+    [InlineData("-  1+2", 1)]
+    [InlineData("-1+ - 2", -3)]
+    public void BasicTest(string formula, int expected)
     {
         var actual = formula.GetChars().Tokenize().Parse().Result;
         Assert.Equal(expected, actual);
